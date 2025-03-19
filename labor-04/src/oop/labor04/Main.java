92e1b779a0c5ce9dc06bc0a74a88622d5e8c85e3
AttilaHegyeli <hegyeli.attila@student.ms.sapientia.ro>
2025-03-19 10:23:50 +0200

    update lab4 - ver1

--- labor-04/src/oop/labor04/Main.java
+++ labor-04/src/oop/labor04/Main.java
@@ -3,5 +3,6 @@ package oop.labor04;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Lab_04");
+        System.out.println("Hegyeli Attila");
     }
 }
