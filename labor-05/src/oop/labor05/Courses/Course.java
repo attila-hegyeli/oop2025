6e2dc8c4103c380803b277b95f3299cadb7ea9e6
AttilaHegyeli <hegyeli.attila@student.ms.sapientia.ro>
2025-03-20 11:23:39 +0200

    Labor5_Course_done_scanner

---
+++ labor-05/src/oop/labor05/Courses/Course.java
@@ -0,0 +1,34 @@
+package oop.labor05.Courses;
+
+public class Course {
+    private final String name;
+    private final String description;
+    private final int numHours;
+
+    public Course(String name, String description, int numHours) {
+        this.name = name;
+        this.description = description;
+        this.numHours = numHours;
+    }
+
+    public int getNumHours() {
+        return numHours;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", numHours=" + numHours +
+                '}';
+    }
+}
--- labor-05/src/oop/labor05/Main.java
+++ labor-05/src/oop/labor05/Main.java
@@ -1,7 +1,46 @@
 package oop.labor05;
 
+import oop.labor05.Courses.Course;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Lab_05");
+        ArrayList<Course> courses = readCourses("courses.csv");
+        for (Course c : courses)
+        {
+            System.out.println(c);
+        }
+    }
+    private static ArrayList<Course> readCourses(String filename)
+    {
+        /// objektum
+        ArrayList<Course> courses = new ArrayList<>();
+        /// Fajl beolvasas!
+        try (Scanner scanner = new Scanner(new File(filename))){
+            while (scanner.hasNextLine()) {
+                /// line lesz a sor
+                String line = scanner.nextLine();
+                /// sor darabolas
+                String[] arr;
+                arr = line.split(",");
+                ///  egy int szam kezelese a bemenetbol
+                int numHours;
+                /// a trim kiszedi a szokozoket
+                numHours = Integer.parseInt(arr[2].trim());
+                /// hozzaadtuk egy sor adatot a tombhoz
+                Course course = new Course(arr[0].trim(),arr[1].trim(),numHours);
+                courses.add(course);
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        /// visszateriti a sort
+        return courses;
     }
 }
