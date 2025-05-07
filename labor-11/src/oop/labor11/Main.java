b32a9f554b28f36d91b0a8f5b7ddd35dd088df5f
AttilaHegyeli <hegyeli.attila@student.ms.sapientia.ro>
2025-05-07 11:45:51 +0300

    Binary_search_Product_labor11

--- labor-11/src/oop/labor11/Main.java
+++ labor-11/src/oop/labor11/Main.java
@@ -1,7 +1,42 @@
 package oop.labor11;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Lab_11");
+//        System.out.println(checkFile("data10.txt"));
+//        System.out.println(doIt("data10.txt","update10.txt"));
+        for (int i = 10; i <= 1000000; i*=10) {
+            String dataFile="data"+i+".txt";
+            for (int j = 10; j <= 10000000; j*=10) {
+                String updateFile="update"+j+".txt";
+                if(checkFile(dataFile) && checkFile(updateFile))
+                {
+                    System.out.println("DataFile: "+dataFile+" UpdateFile: "+updateFile+" Time:"+doIt(dataFile,updateFile));
+                }
+            }
+        }
+
     }
+
+    private static boolean checkFile(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            return true;
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+    }
+
+    private static long doIt(String dataFile, String updateFile) {
+        long startMilli = System.currentTimeMillis();
+        Storage storage = new Storage(dataFile);
+        storage.Update(updateFile);
+
+        long stopMilli = System.currentTimeMillis();
+        return stopMilli - startMilli;
+    }
+
+
 }
---
+++ labor-11/src/oop/labor11/Storage.java
@@ -0,0 +1,67 @@
+package oop.labor11;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    ArrayList<Product> products = new ArrayList<>();
+
+    public Storage(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                String[] data = line.split(" ");
+                int id = Integer.parseInt(data[0].trim());
+                String name = data[1].trim();
+                int amount = Integer.parseInt(data[2].trim());
+                int price = Integer.parseInt(data[3].trim());
+                products.add(new Product(id, name, amount, price));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int Update(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                String[] updateData = line.split(" ");
+                int id = Integer.parseInt(updateData[0].trim());
+                int newAmount = Integer.parseInt(updateData[1].trim());
+                int index = binarySearch(products,0,products.size()-1,id);
+                if(index != -1) {
+                    products.get(index).increaseAmount(newAmount);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int binarySearch(ArrayList<Product> products, int l, int r, int id) {
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (products.get(m).getId() == id) {
+                return m;
+            }
+            if (products.get(m).getId() < id) {
+                l = m + 1;
+
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+}
